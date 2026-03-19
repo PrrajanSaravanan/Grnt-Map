b54d6ce6639008f8c15dc6741e0f3900359ef0e6
MuthuTej <muthu1410raj@gmail.com>
2026-03-19 21:24:43 +0530

    prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname, '');
-  const apiUrl = env.VITE_API_URL || 'http://localhost:3001';
+  const apiUrl = 'https://grnt-map-1.onrender.com/';
   return {
     plugins: [react(), tailwindcss()],
     define: {
